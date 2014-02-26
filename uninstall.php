3d59136ab60d00432edbbd67ddb68d73919a47c0
fpcorso <fpcorso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-26 23:09:26 +0000

    Adding trunk folder for 0.1.1
    git-svn-id: https://plugins.svn.wordpress.org/testimonial-master/trunk@865798 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,7 @@
+<?php
+global $wpdb;
+$table_name = $wpdb->prefix . "mlw_tm_testimonials";
+$sql = "DROP TABLE IF EXISTS ".$table_name;
+$results = $wpdb->query( $sql );
+delete_option('mlw_quiz_master_version');
+?>
